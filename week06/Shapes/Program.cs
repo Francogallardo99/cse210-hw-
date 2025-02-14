2019eebbbbfeb758578dcc6cc3d8df1e83e914ec
Francogallardo99 <gal24016@byui.edu>
2025-02-13 21:16:57 -0300

    Ready

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Square("Green", 5));
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+
+public abstract class Shape
+{
+    private string _color;
+    
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public string SetColor(string color)
+    {
+        _color = color;
+        return _color;
+    }
+    public abstract double GetArea();
+}
